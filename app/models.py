da3b326d04802d91cdfd3f8b66220c6c182039d5
AppMotoristaPro <appmotoristapro@gmail.com>
2026-05-05 09:57:04 -0300

    teste

--- app/models.py
+++ app/models.py
@@ -22,7 +22,7 @@ class User(db.Model, UserMixin):
     data_cadastro = db.Column(db.DateTime, default=lambda: datetime.now(tz_br))
     matricula = db.Column(db.String(20), unique=True, nullable=True)
     
-    # CAMPOS DOCUSIGN
+    # CAMPOS AUTENTIQUE
     termo_assinado = db.Column(db.Boolean, default=False)
     docusign_envelope_id = db.Column(db.String(100), nullable=True)
     
